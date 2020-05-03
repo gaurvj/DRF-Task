fb19cfe534f2b1a41c17cb884fd7cb54e9e5b799
Vijay Gaur <45205779+gaurvj@users.noreply.github.com>
2020-05-04 00:21:38 +0530

    Update views.py

--- mytask/users/views.py
+++ mytask/users/views.py
@@ -5,24 +5,22 @@ import json
 
 #To get all the activity log of users.
 def UserActivtiyDetails(request):
-    try:
-        members = []
-        for i in UserInfo.objects.all():
-            activity = []
-            for j in ActivityPeriod.objects.filter(user_id=i.id):
-                activity.append({
-                    "start_time": j.start_time,
-                    "end_time": j.end_time
-                })
-            members.append({
-                "id": i.id,
-                "real_name": i.real_name,
-                "tz": i.tz,
-                "activity_periods": activity
+    members = []
+    for i in UserInfo.objects.all():
+        activity = []
+        for j in ActivityPeriod.objects.filter(user_id=i.id):
+            activity.append({
+                "start_time": j.start_time,
+                "end_time": j.end_time
             })
+        members.append({
+            "id": i.id,
+            "real_name": i.real_name,
+            "tz": i.tz,
+            "activity_periods": activity
+        })
 
-        result = {"ok": True,
-                  "members": members}
-        return HttpResponse(json.dumps(result))
-    except Exception as e:
-        print(e)
+    result = {"ok": True,
+              "members": members}
+    return HttpResponse(json.dumps(result))
+   
